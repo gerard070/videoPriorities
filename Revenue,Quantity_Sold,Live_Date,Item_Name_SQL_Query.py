5e07391d7c686961bd88471a6659d95d654838dd
Gerard <geconn117@gmail.com>
2018-07-12 11:11:45 -0400

    Final Working File
    Live_items.name was missing from a subquery, and so name in the final select did not work.
    
    This works now.

--- Revenue,Quantity_Sold,Live_Date,Item_Name_SQL_Query.py
+++ Revenue,Quantity_Sold,Live_Date,Item_Name_SQL_Query.py
@@ -32,38 +32,38 @@ last_year = datetime(today_date.year-1, today_date.month, today_date.day)
 #this is the query
 q = """
                                                                                                                             
-select item_id, sum(sum_of_sales) as Total_Sales, sum(units_sold) as Total_Quantity_sold, live_date::date , name from       
-(                                                                                                                           
-                                                                                                                            
-Select live_items.item_id, case when sum_sales.sum is null then 0 else sum_sales.sum end Sum_of_Sales, 
-case when sum_sales.units_sold is null then 0 else sum_sales.units_sold End Units_sold, live_items.live_date
+select item_id, sum(sum_of_sales) as Total_Sales, sum(units_sold) as Total_Quantity_sold, live_date::date , name from
+(
 
-from                                                                                                                        
-                                                                                                                            
-                                                                                                                            
-((select sum(os.price*os.quantity), sum(os.quantity) as Units_sold, left(sku::VARCHAR, 5)  as item_number                   
-from orders o                                                                                                               
-join order_sku os on o.order_id = os.order_id                                                                               
-                                                                                                                            
-                                                                                                                            
-                                                                                                                            
-where o.is_cancelled != 1                                                                                                   
-group by  left(sku::VARCHAR, 5))                                                                                            
-union ALL                                                                                                                   
- (select sum(oso.price*oso.quantity), sum(oso.quantity) as Units_sold, left(sku::VARCHAR, 5)  as item_number                
-from orders_old oo                                                                                                          
-join order_sku_old oso on oo.order_id = oso.order_id                                                                        
- where oo.is_cancelled != 1                                                                                                 
-group by  left(sku::VARCHAR, 5))) sum_sales                                                                                 
-right join                                                                                                                  
-    (select i.item_id, i.live_date , i.name from item i                                                                     
-where live_date is not null                                                                                                 
-and status_id = 1) as Live_items                                                                                            
-    on Live_items.item_id = sum_sales.item_number::NUMERIC                                                                  
-                                                                                                                            
+Select live_items.item_id, case when sum_sales.sum is null then 0 else sum_sales.sum end Sum_of_Sales,
+case when sum_sales.units_sold is null then 0 else sum_sales.units_sold End Units_sold, live_items.live_date, Live_items.name
+
+from
+
+
+((select sum(os.price*os.quantity), sum(os.quantity) as Units_sold, left(sku::VARCHAR, 5)  as item_number
+from orders o
+join order_sku os on o.order_id = os.order_id
+
+
+
+where o.is_cancelled != 1
+group by  left(sku::VARCHAR, 5))
+union ALL
+ (select sum(oso.price*oso.quantity), sum(oso.quantity) as Units_sold, left(sku::VARCHAR, 5)  as item_number
+from orders_old oo
+join order_sku_old oso on oo.order_id = oso.order_id
+ where oo.is_cancelled != 1
+group by  left(sku::VARCHAR, 5))) sum_sales
+right join
+    (select i.item_id, i.live_date , i.name from item i
+where live_date is not null
+and status_id = 1) as Live_items
+    on Live_items.item_id = sum_sales.item_number::NUMERIC
+
 group by  live_items.item_id, sum_sales.sum, sum_sales.units_sold, live_items.live_date, Live_items.name) all_Sales_Data_For_Live_items
-where item_id != 99999                                                                                                      
-group by all_Sales_Data_For_Live_Items.item_id, all_Sales_Data_For_Live_Items.live_date, all_Sales_Data_For_Live_Items.name 
+where item_id != 99999
+group by all_Sales_Data_For_Live_Items.item_id, all_Sales_Data_For_Live_Items.live_date, all_Sales_Data_For_Live_Items.name
 
 
 """
